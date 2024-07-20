5d021bbf13fdc5b11e7ec9b6de2ef00f6ca08385
Гавриш Олександр <synertech2023@gmail.com>
2024-07-20 09:05:56 +0300

    Update script.js

--- script.js
+++ script.js
@@ -16,9 +16,20 @@ app.use(cors({
 // Middleware для парсингу JSON
 app.use(bodyParser.json());
 
+// Middleware для налаштування заголовків CORS
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://synertech-21d84.web.app');
+  res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Credentials', 'true');
+    return res.status(200).json({});
+  }
+  next();
+});
+
 // Підключення до MongoDB
 mongoose.connect('mongodb+srv://Alex_Gavrish:IKc0xvjnoOshP9Vp@cluster0.sdvypro.mongodb.net/synertech?retryWrites=true&w=majority')
-
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.error('MongoDB connection error:', err));
 
@@ -33,7 +44,7 @@ const userSchema = new mongoose.Schema({
 const User = mongoose.model('User', userSchema);
 
 // Маршрут для реєстрації користувачів
-app.post('', async (req, res) => {
+app.post('/', async (req, res) => {
   const { name, email, select, project } = req.body;
 
   const newUser = new User({ name, email, select, project });
